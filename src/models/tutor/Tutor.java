cc6ef73a439eb7e25c558e29dad448babcb9a58d
emimartin <emimartin26@gmail.com>
2015-04-13 17:35:25 -0300

    Agregue clase mutual y tutor

--- src/models/tutor/Tutor.java
+++ src/models/tutor/Tutor.java
@@ -6,10 +6,53 @@
 
 package models.tutor;
 
+import java.util.Set;
+import models.telefono.Telefono;
+import models.ubicacion.Direccion;
+
 /**
  *
  * @author emiliano
  */
 public class Tutor {
+    
+    private String nombre;
+    private String apellido;
+    private Direccion direccion;
+    private Set<Telefono> telefonos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    public Set<Telefono> getTelefonos() {
+        return telefonos;
+    }
+
+    public void setTelefonos(Set<Telefono> telefonos) {
+        this.telefonos = telefonos;
+    }
+    
+    
     
 }
